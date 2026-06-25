9c7843017c7d252b0b2985f80ad5998d92831ec5
MrZece <157902787+MrZece@users.noreply.github.com>
2026-06-25 03:47:13 +0300

    runner 99% complete

--- hopper's-trap/levels/levelrunner/trapspawner.gd
+++ hopper's-trap/levels/levelrunner/trapspawner.gd
@@ -1,1 +1,17 @@
 extends Node2D
+
+@onready var ground: Node2D = $going
+
+const BEARTRAP = preload("uid://b4qbbdekef3q3")
+var speed=150
+
+func _process(delta: float) -> void:
+	for kids in ground.get_children():
+		kids.global_position.x-=speed*delta
+
+func _on_timer_timeout() -> void:
+	for i in range(randi_range(1,2)):
+		var trap=BEARTRAP.instantiate()
+		ground.add_child(trap)
+		trap.time=5
+		trap.global_position=Vector2(ground.global_position.x,randf_range(240,320))
--- hopper's-trap/reusable/traps/beartraping.gd
+++ hopper's-trap/reusable/traps/beartraping.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+var time:int
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("plr"):
 		animation.play("catch")
+
+func _on_timer_timeout() -> void:
+	time-=1
+	if time==0:
+		queue_free()
