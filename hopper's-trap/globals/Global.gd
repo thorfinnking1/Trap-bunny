4a3c07fdfabc785bd38ad34377cd4a6fe48ed64e
MrZece <157902787+MrZece@users.noreply.github.com>
2026-06-29 21:33:20 +0300

    thing a lot of stuff

--- hopper's-trap/globals/Global.gd
+++ hopper's-trap/globals/Global.gd
@@ -3,4 +3,6 @@ extends Node
 var player_position:Vector2i
 
 var levels:Dictionary[int,Array]={0:["[b][color=black][wave]hop-n-run","[b][color=green][wave]easy",0],
-								1:["[b][color=black][wave]fight idk dani you choose","[b][rainbow][wave]GUY GAY GUN",3]}
+								1:["[b][color=black][wave]fight idk dani you choose","[b][rainbow][wave]GUY GAY GUN",3],
+								2:["[b][color=black][wave]hop-n-run 2","[b][color=blue][wave]mid",0],
+								3:["[b][color=black][wave]can't bear","[b][color=green][wave]easy",0]}
